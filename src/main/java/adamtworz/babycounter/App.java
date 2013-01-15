4319585c4da81fadd1439792ee17450f03f1aa8a
s9904 <s9904@pjwstk.edu.pl>
2013-01-15 22:26:17 +0100

    first commit

---
+++ src/main/java/adamtworz/babycounter/App.java
@@ -0,0 +1,13 @@
+package adamtworz.babycounter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World1!" );
+    }
+}
